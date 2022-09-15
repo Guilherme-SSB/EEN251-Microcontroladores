5649f5e6ee344c7f8aa2561b60dcc4236e8224c5
Guilherme Samuel de Souza Barbosa <gui.samuel10@gmail.com>
2022-09-15 17:22:43 -0300

    Update main.py

--- codigos/main.py
+++ codigos/main.py
@@ -1,49 +1,51 @@
-from time import sleep_ms
-
-from buzzer import BUZZER
-from dth11 import DTH22
-from ldr import LDR
-from mq2 import MQ2
 from oled import OLED
-
+from dth11 import DTH22
+from buzzer import BUZZER
+from ldr import LDR
+from time import sleep_ms
+from mq2 import MQ2
+import json
 
 def main():
-
-    # Instancia todos os sensores utilizados
     meu_dth22 = DTH22()
     meu_buzzer = BUZZER()
     meu_ldr = LDR()
     meu_oled = OLED()
     meu_mq2 = MQ2(pinData = 26, baseVoltage = 3.3)
-
-    # Calibra o sensor de gás
     print("Calibrando")
     meu_mq2.calibrate()
-    print("Calibragem finalizada")
+    print("Calibração completa")
     print("Resistência base:{0}".format(meu_mq2._ro))
     
-    # Limpa o display OLED
     meu_oled.limpar()
-
-    # Variáveis de som, que serão utilizadas em situações aqui consideradas críticas
+    # song = ["E5","G5","A5","P","E5","G5","B5","A5","P","E5","G5","A5","P","G5","E5"]
     song_ldr = ["E5","G5","A5"]
     song_mq2 = ["G5","C5","G5","C5"]
     
-    # Loop infinito
     while True:
-        # Leitura da temperatura e da umidade e exibição na tela
-        meu_oled.exibir(str(meu_dth22.medirTemp()), 2,15)
-        meu_oled.exibir(str(meu_dth22.medirUmidade()), 5, 35)
+        #meu_oled.exibir(str(meu_dth22.medirTemp()), 2,15)
+        #meu_oled.exibir(str(meu_dth22.medirUmidade()), 5, 35)
         
-        # Leitura do sensor de luz e de fumaça e print no terminal
-        print("LDR: {:.1f}".format(meu_ldr.read())+" - ", end='')
-        print("Fumaça: {:.1f} ppm".format(meu_mq2.readSmoke()))
+        # try:
+        temp = float(meu_dth22.medirTemp())
+        sleep_ms(2000)
+        umidade = meu_dth22.medirUmidade()
         
-        # Se o sensor de luz estiver abaixo do limite (10000), toca o buzzer
+        ldr = float(meu_ldr.read())
+        fumaca = round(float(meu_mq2.readSmoke()),1)
+        #print('Temperatura: ', temp, end=' ')
+        #print(' - Umidade: ', umidade, end=' ')
+        #print(" - LDR: {:.1f}".format(meu_ldr.read()), end='')
+        #print(" - Fumaça: {:.1f}".format(meu_mq2.readSmoke()))
+        
+        saida = {"Temperatura":temp, "Umidade":umidade, "LDR": ldr, "Fumaca": fumaca}
+        saida2 = json.dumps(saida)
+        print(saida2)
+        
+        
         if meu_ldr.read() <= 10000:
            meu_buzzer.playsong(song_ldr)
         
-        # Se o sensor de fumaça estiver acima do limite (25), toca o buzzer
         if meu_mq2.readSmoke() > 25:
             meu_buzzer.playsong(song_mq2)
         
@@ -51,4 +53,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
