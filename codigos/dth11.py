54d58a605a3b6569a5792e6e15b4d894239b8b3e
Guilherme Samuel de Souza Barbosa <gui.samuel10@gmail.com>
2022-09-15 17:22:26 -0300

    Update dth11.py

--- codigos/dth11.py
+++ codigos/dth11.py
@@ -1,32 +1,26 @@
-from time import sleep_ms
-
 import dht
 import machine
-
+from time import sleep_ms
 
 class DTH22:
     def __init__(self):
-        """
-        Seta o pino do DTH22
-        """
         SENSOR_PIN = 28
-        self.sensor = dht.DHT22(machine.Pin(
-            SENSOR_PIN, machine.Pin.IN, machine.Pin.PULL_UP))
+        self.sensor = dht.DHT22(machine.Pin(SENSOR_PIN, machine.Pin.IN, machine.Pin.PULL_UP))
 
     def medirTemp(self) -> str:
-        """
-        Lê a temperatura do sensor e retorna uma string com a temperatura
-        :return: The temperature in Celsius.
-        """
         self.sensor.measure()
         sleep_ms(100)
-        temperatura = "Temp: " + str(self.sensor.temperature()) + "C"
+        # temperatura = "Temp: " + str(self.sensor.temperature()) + "C"
+        temperatura = str(self.sensor.temperature())
+        
         return temperatura
-
+    
     def medirUmidade(self) -> str:
-        """
-        Lê a umidade do sensor e retorna uma string com o valor
-        """
         self.sensor.measure()
-        umidade = "Umidade: " + str(self.sensor.humidity()) + "%"
+        # umidade = "Umidade: " + str(self.sensor.humidity()) + "%"
+        umidade = self.sensor.humidity()
         return umidade
+
+#dht = DTH22()
+#temp = dht.medirUmidade()
+#print(temp)
